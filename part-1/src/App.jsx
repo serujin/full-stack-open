50a3e514ea54d137eea84f4f440d3c4711fb8eb5
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-01-07 13:04:42 +0100

    feat: part 1 exercise 8

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -10,37 +10,52 @@ const Display = ({ text, value }) => (
   </p>
 );
 
+const Statistics = ({ goodData, neutralData, badData }) => {
+  const allFeedback = goodData.value + neutralData.value + badData.value;
+  const average = goodData.value / allFeedback - badData.value / allFeedback;
+  const positivePercentage = (goodData.value / allFeedback) * 100;
+  const positivePercentageText = `${positivePercentage} %`;
+  const zeroPercentageText = "0 %";
+
+  return (
+    <>
+      <Title text={"Statistics"} />
+      <Display text={goodData.text} value={goodData.value} />
+      <Display text={neutralData.text} value={neutralData.value} />
+      <Display text={badData.text} value={badData.value} />
+      <Display text={"All"} value={allFeedback} />
+      <Display text={"Average"} value={average ? average : 0} />
+      <Display
+        text={"Positive"}
+        value={positivePercentage ? positivePercentageText : zeroPercentageText}
+      />
+    </>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const goodText = "Good";
-  const neutralText = "Neutral";
-  const badText = "Bad";
-
-  const allFeedback = good + neutral + bad;
-  const average = good / allFeedback - bad / allFeedback;
-  const positivePercentage = (good / allFeedback) * 100;
-  const positivePercentageText = `${positivePercentage} %`;
-  const zeroPercentageText = "0 %";
+  const goodData = { text: "Good", value: good };
+  const neutralData = { text: "Neutral", value: neutral };
+  const badData = { text: "Bad", value: bad };
 
   return (
     <div>
       <Title text={"Give feedback"} />
-      <Button handleClick={() => setGood(good + 1)} text={goodText} />
-      <Button handleClick={() => setNeutral(neutral + 1)} text={neutralText} />
-      <Button handleClick={() => setBad(bad + 1)} text={badText} />
-      <Title text={"Statistics"} />
-      <Display text={goodText} value={good} />
-      <Display text={neutralText} value={neutral} />
-      <Display text={badText} value={bad} />
-      <Display text={"All"} value={allFeedback} />
-      <Display text={"Average"} value={average ? average : 0} />
-      <Display
-        text={"Positive"}
-        value={positivePercentage ? positivePercentageText : zeroPercentageText}
+      <Button handleClick={() => setGood(good + 1)} text={goodData.text} />
+      <Button
+        handleClick={() => setNeutral(neutral + 1)}
+        text={neutralData.text}
+      />
+      <Button handleClick={() => setBad(bad + 1)} text={badData.text} />
+      <Statistics
+        goodData={goodData}
+        neutralData={neutralData}
+        badData={badData}
       />
     </div>
   );
