4938dedb69b0e9c8e27a3f7b36f0a843e7de4505
Sergio Muñoz <sergio.munoz.lillo@gmail.com>
2024-01-08 18:30:59 +0100

    feat: part 2 exercise 1

---
+++ part-2/src/App.jsx
@@ -0,0 +1,50 @@
+const Title = ({ text }) => <h1>{text}</h1>;
+
+const CourseContent = ({ part }) => {
+  const { exercises, name } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Course = ({ course }) => {
+  const { name, parts } = course;
+  return (
+    <>
+      <Title text={name} />
+      {parts.map(({ id, ...part }) => (
+        <CourseContent key={id} part={part} />
+      ))}
+    </>
+  );
+};
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
